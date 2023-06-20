480d0b71d2af2e123f0373497b860638c0fd0604
Roopal-demo <63741128+Roopal-demo@users.noreply.github.com>
2023-06-20 11:48:28 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,6 +11,7 @@ def scrape():
     headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date","hyperlink"]
     planet_data = []
     for i in range(0, 2):
+        time.sleep(2)
         soup = BeautifulSoup(browser.page_source, "html.parser")
         for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
             li_tags = ul_tag.find_all("li")
